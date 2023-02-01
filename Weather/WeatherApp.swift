aa7b89c0158ee33c9c530b65e037128faeb0a634
Roma <moshkovcevroma@gmail.com>
2023-02-01 13:45:43 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Роман Мошковцев on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
